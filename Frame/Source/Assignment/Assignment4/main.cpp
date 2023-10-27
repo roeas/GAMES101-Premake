ecd8c747e0a26b6c74f77abdac8f7fc208d1ded4
Hinageshi01 <2247001665@qq.com>
2023-10-27 17:28:45 +0800

    Bezier

--- Frame/Source/Assignment/Assignment4/main.cpp
+++ Frame/Source/Assignment/Assignment4/main.cpp
@@ -1,15 +1,19 @@
 #include <chrono>
 #include <iostream>
+
+#include<eigen3/Eigen/Core>
+#include<eigen3/Eigen/Dense>
 #include <opencv2/opencv.hpp>
+
+constexpr size_t POITN_COUNT = 4;
 
 std::vector<cv::Point2f> control_points;
 
 void mouse_handler(int event, int x, int y, int flags, void *userdata) 
 {
-    if (event == cv::EVENT_LBUTTONDOWN && control_points.size() < 4) 
+    if (event == cv::EVENT_LBUTTONDOWN && control_points.size() < POITN_COUNT)
     {
-        std::cout << "Left button of the mouse is clicked - position (" << x << ", "
-        << y << ")" << '\n';
+        std::cout << "Mouse clicked: (" << x << ", " << y << ")" << std::endl;
         control_points.emplace_back(x, y);
     }     
 }
@@ -30,18 +34,59 @@ void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window)
     }
 }
 
+inline cv::Point2f LerpPoint2f(const cv::Point2f &a, const cv::Point2f &b, float t)
+{
+    return a + t * (b - a);
+}
+
 cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, float t) 
 {
-    // TODO: Implement de Casteljau's algorithm
-    return cv::Point2f();
+    if (control_points.size() == 2)
+    {
+        return LerpPoint2f(control_points[0], control_points[1], t);
+    }
 
+    std::vector<cv::Point2f> temp;
+    for (size_t i = 0; i < control_points.size() - 1; ++i)
+    {
+        temp.emplace_back(LerpPoint2f(control_points[i], control_points[i + 1], t));
+    }
+
+    return recursive_bezier(temp, t);
 }
 
 void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window) 
 {
-    // TODO: Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's 
-    // recursive Bezier algorithm.
+    float filter[3][3] = {
+        0.2f, 0.5f, 0.2f,
+        0.5f, 1.0f, 0.5f,
+        0.2f, 0.5f, 0.2f
+    };
 
+    Eigen::Vector2i offset[3][3] = {
+        Eigen::Vector2i{-1, 1}, Eigen::Vector2i{0, 1}, Eigen::Vector2i{1, 1},
+        Eigen::Vector2i{-1, 0}, Eigen::Vector2i{0, 0}, Eigen::Vector2i{1, 0},
+        Eigen::Vector2i{-1, -1}, Eigen::Vector2i{0, -1}, Eigen::Vector2i{1, -1},
+    };
+
+    for (float t = 0.0; t <= 1.0; t += 0.001)
+    {
+        cv::Point2f p = recursive_bezier(control_points, t);
+        Eigen::Vector2i point{ p.x, p.y };
+
+        for (size_t i = 0; i < 3; ++i)
+        {
+            for (size_t j = 0; j < 3; ++j)
+            {
+                int x_index = point.x() + offset[i][j].x();
+                int y_index = point.y() + offset[i][j].y();
+
+                // OpenVC 会使一个像素点的值 mod 255
+                uchar color = std::max(static_cast<uchar>(filter[i][j] * 255), window.at<cv::Vec3b>(y_index, x_index)[1]);
+                window.at<cv::Vec3b>(y_index, x_index)[1] = std::min(static_cast<uchar>(255), color);
+            }
+        }
+    }
 }
 
 int main() 
@@ -49,7 +94,6 @@ int main()
     cv::Mat window = cv::Mat(700, 700, CV_8UC3, cv::Scalar(0));
     cv::cvtColor(window, window, cv::COLOR_BGR2RGB);
     cv::namedWindow("Bezier Curve", cv::WINDOW_AUTOSIZE);
-
     cv::setMouseCallback("Bezier Curve", mouse_handler, nullptr);
 
     int key = -1;
@@ -57,23 +101,17 @@ int main()
     {
         for (auto &point : control_points) 
         {
-            cv::circle(window, point, 3, {255, 255, 255}, 3);
+            cv::circle(window, point, 3, { 255, 255, 255 }, 3);
         }
 
-        if (control_points.size() == 4) 
+        if (control_points.size() == POITN_COUNT)
         {
-            naive_bezier(control_points, window);
-            //   bezier(control_points, window);
-
-            cv::imshow("Bezier Curve", window);
-            cv::imwrite("my_bezier_curve.png", window);
-            key = cv::waitKey(0);
-
-            return 0;
+            // naive_bezier(control_points, window);
+            bezier(control_points, window);
         }
 
         cv::imshow("Bezier Curve", window);
-        key = cv::waitKey(20);
+        key = cv::waitKey(1);
     }
 
 return 0;
