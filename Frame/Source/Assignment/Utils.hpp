a17f55bfd47c8fe844ed8037da3dfa96b106dda0
Hinageshi01 <2247001665@qq.com>
2023-12-02 17:52:10 +0800

    FloatEqual EPSILON parameter

--- Frame/Source/Assignment/Utils.hpp
+++ Frame/Source/Assignment/Utils.hpp
@@ -12,9 +12,9 @@ inline std::string PathFromAsset(std::string relativePath)
     return (std::filesystem::path(FRAME_ASSET_PATH) / std::move(relativePath)).generic_string();
 }
 
-inline bool FloatEqual(const float a, const float b)
+inline bool FloatEqual(const float a, const float b, const float EPSILON = FLT_EPSILON)
 {
-    return (std::abs(a - b) <= FLT_EPSILON);
+    return (std::abs(a - b) <= EPSILON);
 }
 
 } // namespace Utils
