6ce240fffdcf4d0d9d88e9ac6321c25a3edf32d9
Hinageshi01 <2247001665@qq.com>
2023-12-26 16:23:06 +0800

    Assignment8

--- Frame/Source/Assignment/Assignment8/application.h
+++ Frame/Source/Assignment/Assignment8/application.h
@@ -17,51 +17,55 @@
 
 using namespace std;
 
-namespace CGL {
+namespace CGL
+{
 
-struct AppConfig {
-  AppConfig() {
-    // Rope config variables
-    mass = 1;
-    ks = 100;
+struct AppConfig
+{
+    AppConfig()
+    {
+// Rope config variables
+        mass = 1;
+        ks = 100;
 
-    // Environment variables
-    gravity = Vector2D(0, -1);
-    steps_per_frame = 64;
-  }
+        // Environment variables
+        gravity = Vector2D(0, -1);
+        steps_per_frame = 1024;
+    }
 
-  float mass;
-  float ks;
+    float mass;
+    float ks;
 
-  float steps_per_frame;
-  Vector2D gravity;
+    float steps_per_frame;
+    Vector2D gravity;
 };
 
-class Application : public Renderer {
+class Application : public Renderer
+{
 public:
-  Application(AppConfig config);
-  ~Application();
+    Application(AppConfig config);
+    ~Application();
 
-  void init();
-  void render();
-  void resize(size_t w, size_t h);
+    void init();
+    void render();
+    void resize(size_t w, size_t h);
 
-  std::string name();
-  std::string info();
+    std::string name();
+    std::string info();
 
-  void keyboard_event(int key, int event, unsigned char mods);
-  // void cursor_event(float x, float y);
-  // void scroll_event(float offset_x, float offset_y);
-  // void mouse_event(int key, int event, unsigned char mods);
+    void keyboard_event(int key, int event, unsigned char mods);
+    // void cursor_event(float x, float y);
+    // void scroll_event(float offset_x, float offset_y);
+    // void mouse_event(int key, int event, unsigned char mods);
 
 private:
-  AppConfig config;
+    AppConfig config;
 
-  Rope *ropeEuler;
-  Rope *ropeVerlet;
+    Rope *ropeEuler;
+    Rope *ropeVerlet;
 
-  size_t screen_width;
-  size_t screen_height;
+    size_t screen_width;
+    size_t screen_height;
 
 }; // class Application
 
--- Frame/Source/Assignment/Assignment8/rope.cpp
+++ Frame/Source/Assignment/Assignment8/rope.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 #include <vector>
 
@@ -7,55 +8,75 @@
 #include "rope.h"
 #include "spring.h"
 
-namespace CGL {
+namespace CGL
+{
 
-    Rope::Rope(Vector2D start, Vector2D end, int num_nodes, float node_mass, float k, vector<int> pinned_nodes)
+Rope::Rope(Vector2D start, Vector2D end, int num_nodes, float node_mass, float k, vector<int> pinned_nodes)
+{
+    masses.reserve(num_nodes);
+    springs.reserve(num_nodes - 1);
+
+    for (size_t i = 0; i < num_nodes; ++i)
     {
-        // TODO (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
-
-//        Comment-in this part when you implement the constructor
-//        for (auto &i : pinned_nodes) {
-//            masses[i]->pinned = true;
-//        }
+        Vector2D pos = start + (end - start) * (static_cast<float>(i) / (static_cast<float>(num_nodes) - 1.0f));
+        masses.emplace_back(new Mass{ pos, node_mass, false });
     }
 
-    void Rope::simulateEuler(float delta_t, Vector2D gravity)
+    for (size_t i = 0; i < num_nodes - 1; ++i)
     {
-        for (auto &s : springs)
-        {
-            // TODO (Part 2): Use Hooke's law to calculate the force on a node
-        }
-
-        for (auto &m : masses)
-        {
-            if (!m->pinned)
-            {
-                // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
-
-                // TODO (Part 2): Add global damping
-            }
-
-            // Reset all forces on each mass
-            m->forces = Vector2D(0, 0);
-        }
+        springs.emplace_back(new Spring{ masses[i], masses[i + 1], k });
     }
 
-    void Rope::simulateVerlet(float delta_t, Vector2D gravity)
+    for (const auto &i : pinned_nodes)
     {
-        for (auto &s : springs)
+        assert(i < masses.size());
+        masses[i]->pinned = true;
+    }
+}
+
+void Rope::simulateEuler(float delta_t, Vector2D gravity)
+{
+    for (const auto &s : springs)
+    {
+        float length = (s->m1->position - s->m2->position).norm();
+        s->m1->forces += -s->k * (s->m1->position - s->m2->position) / length * (length - s->rest_length);
+        s->m2->forces += -s->k * (s->m2->position - s->m1->position) / length * (length - s->rest_length);
+    }
+
+    for (auto &m : masses)
+    {
+        if (!m->pinned)
         {
-            // TODO (Part 3): Simulate one timestep of the rope using explicit Verlet （solving constraints)
+            m->velocity += (m->forces / m->mass + gravity) * delta_t;
+            m->position += m->velocity * delta_t;
         }
 
-        for (auto &m : masses)
-        {
-            if (!m->pinned)
-            {
-                Vector2D temp_position = m->position;
-                // TODO (Part 3.1): Set the new position of the rope mass
-                
-                // TODO (Part 4): Add global Verlet damping
-            }
-        }
+        m->forces = Vector2D{ 0, 0 };
     }
+}
+
+void Rope::simulateVerlet(float delta_t, Vector2D gravity)
+{
+    for (auto &s : springs)
+    {
+        auto length = (s->m1->position - s->m2->position).norm();
+        s->m1->forces += -s->k * (s->m1->position - s->m2->position) / length * (length - s->rest_length);
+        s->m2->forces += -s->k * (s->m2->position - s->m1->position) / length * (length - s->rest_length);
+    }
+
+    for (auto &m : masses)
+    {
+        if (!m->pinned)
+        {
+            constexpr float friction = 0.00001;
+
+            Vector2D position = m->position;
+            m->position = position + (1.0f - friction) * (position - m->last_position) + (m->forces / m->mass + gravity) * delta_t * delta_t;
+            m->last_position = position;
+        }
+
+        m->forces = Vector2D{ 0, 0 };
+    }
+}
+
 }
