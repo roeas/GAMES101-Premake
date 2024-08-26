c6a119ee01be17ca0a106d4701d076bfab587e52
Hinageshi <69386319+roeas@users.noreply.github.com>
2024-08-26 17:19:18 +0800

    Regular merge (#21)

--- Frame/Source/Assignment/Utils.hpp
+++ Frame/Source/Assignment/Utils.hpp
@@ -1,20 +1,18 @@
 #pragma once
 
-#include <cfloat>
 #include <filesystem>
-#include <string>
 
 namespace Utils
 {
 
-inline std::string PathFromRoot(std::string relativePath)
+inline std::string PathFromRoot(std::string_view relativePath)
 {
-    return (std::filesystem::path(FRAME_ROOT_PATH) / std::move(relativePath)).generic_string();
+    return (std::filesystem::path(FRAME_ROOT_PATH) / relativePath).generic_string();
 }
 
-inline std::string PathFromAsset(std::string relativePath)
+inline std::string PathFromAsset(std::string_view relativePath)
 {
-    return (std::filesystem::path(FRAME_ASSET_PATH) / std::move(relativePath)).generic_string();
+    return (std::filesystem::path(FRAME_ASSET_PATH) / relativePath).generic_string();
 }
 
 inline bool FloatEqual(const float a, const float b, const float EPSILON = FLT_EPSILON)
